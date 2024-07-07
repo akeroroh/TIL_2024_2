6d4edd5d70992f4660c536fa8a35f33b436ee2ab
akeroroh <jah000118@naver.com>
2024-07-07 23:24:11 +0900

    77

--- React/7.7/40강/index.js
+++ React/7.7/40강/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import AttendanceBook from './chapter_10/AttendanceBook';
+import Calculator from './chapter_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <Calculator />
   </React.StrictMode>
 )
 
